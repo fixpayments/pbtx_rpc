8f055164fc1af53d2f66517c38cd6fa813650d55
cc32d9 <cc32d9@gmail.com>
2023-01-01 23:53:43 +0100

    work in progress

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-"use strict";
+'use strict';
 
 const express = require('express');
 require('dotenv-defaults').config();
+const protobuf = require('protobufjs');
+var hash = require('hash.js');
 
 const required_options = ['PORT', 'BINDADDR', 'URL_PATH', 'NETWORK_ID', 'ANTELOPE_RPC', 'ANTELOPE_CONTRACT',
                           'ANTELOPE_ADMIN', 'ANTELOPE_ADMIN_PK', 'ANTELOPE_WORKER', 'ANTELOPE_WORKER_PK'];
@@ -14,9 +16,39 @@ for (const opt of required_options) {
     }
 }
 
+protobuf.load('pbtx-rpc.proto', function(err, root) {
+    if (err) {
+        console.error('Cannot load proto file: ' + err);
+        process.exit(1);
+    }
+});
+
 
 const app = express();
 
+app.use(function (req, res, next) {
+    if( ! req.is('application/octet-stream') ) {
+        throw new Error('Expected application/octet-stream');
+    }
+    next();
+});
+
+app.use(express.raw());
+
+app.use(function (req, res, next) {
+    req.sha256digest = hash.sha256().update(req.body()).digest();
+    next();
+});
+
+
+app.post(process.env.URL_PATH + '/register_account', (req, res) => {
+    var RegisterAccount = protobuf.Root().lookupType('pbtxrpc.RegisterAccount');
+    var msg = RegisterAccount.decodeDelimited(req.body());
+    console.log(msg);
+})
+
+
+
 app.listen(process.env.PORT, process.env.BINDADDR, () => {
-  console.log(`Server is running at http://${process.env.BINDADDR}:${process.env.PORT}`);
+    console.log(`Server is running at http://${process.env.BINDADDR}:${process.env.PORT}`);
 });
